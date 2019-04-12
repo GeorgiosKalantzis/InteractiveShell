6a1095295b422128d7433606b23e6ce64a0caaf4
GeorgiosKalantzis <georgekalantzis997@gmail.com>
2019-04-13 00:36:54 +0300

    Add files via upload

--- myshell.c
+++ myshell.c
@@ -11,6 +11,7 @@
 #define DELIM2 ";"
 #define DELIM3 "&&"
 
+// Read input from stdin
 char *readLine(void){
 
   char *buffer = malloc(sizeof(char) * BUF_SIZE);
@@ -29,7 +30,7 @@ char *readLine(void){
   return buffer;
 
 }
-
+// Split the arguments of the command
 char **splitLine(char *line){
 
   int i = 0 ;
@@ -38,8 +39,8 @@ char **splitLine(char *line){
 
    // Check for allocation error
   if (!tokens) {
-  fprintf(stderr, "myshell: allocation error\n");
-  exit(EXIT_FAILURE);
+    fprintf(stderr, "myshell: allocation error\n");
+    exit(EXIT_FAILURE);
   }
 
   token = strtok(line , DELIM);
@@ -51,6 +52,8 @@ char **splitLine(char *line){
   }
   tokens[i] = NULL;
 
+
+
   return tokens;
 
 }
@@ -61,18 +64,9 @@ int execute(char **args, int fl){
   if(args[0] == NULL){
     return 1;
   }
-
   pid_t pid;
   int status,fd[2];
   int k = 0 ;
-  //int wr =0;
-
-//  char *error = "error";
-  //char *message;
-//  message = malloc(sizeof(char) * (strlen(error) + 1));
-
-  //int a = 5 ;
-  //int b ;
 
   char line[BUF_SIZE];
 
@@ -81,28 +75,22 @@ int execute(char **args, int fl){
     exit(EXIT_FAILURE);
   }
 
+
   pid = fork();
 
   if (pid == 0) {
   // Child process
     if (execvp(args[0], args) == -1) {
       perror("myshelgdfl");
-      //printf("%d\n", fl );
+
       if (fl == 0){
-    //  printf("snek");
-    //  printf("%d\n", fl );
-
-
         close(fd[0]);
-        //printf("%d\n", fl );
-
 
         write( fd[1] , "message through pipe\n", 21);
 
         close(fd[1]);
 
       }
-      //printf("%d\n", k  );
     }
     exit(EXIT_SUCCESS);
 
@@ -112,14 +100,11 @@ int execute(char **args, int fl){
   } else {
     // Parent process
     wait(&status);
-    //printf("%d\n", k );
 
     if ( fl == 0){
-
       close(fd[1]);
 
       k = read(fd[0] , line , BUF_SIZE);
-    //  printf("%d\n", k );
 
       close(fd[0]);
 
@@ -129,7 +114,7 @@ int execute(char **args, int fl){
       return 0 ;
 
     }
-    //printf("gfsi\n");
+
 
   }
 
@@ -150,6 +135,7 @@ int nConsecutivecmds(char *line){
 
   if(strcmp(token,line) == 0){
 
+
     free(copyline);
     return 0 ;
 
@@ -165,10 +151,10 @@ int nConsecutivecmds(char *line){
 
   }
 
+
 }
 
 int consecutiveCmds(char *line , int ncmds){
-
   int stat;
   int fl;
   char **args;
@@ -182,6 +168,38 @@ int consecutiveCmds(char *line , int ncmds){
     fprintf(stderr, "myshell: allocation error\n");
     exit(EXIT_FAILURE);
   }
+
+  int z;
+  int c1 = 0;
+  int c2 = 0;
+  int c3 = 0;
+  for(z = 0 ; z < strlen(line) ; z++){
+
+    if( line[z] == delims[0]){
+      c1 = c1 + 1;
+    }
+
+    if (line[z] == delims[1] && line[z+1] == delims[1]){
+      c2 = c2 + 1;
+
+    }
+
+    if (line[z] == delims[1]){
+      c3 = c3 + 1;
+    }
+
+  }
+  // Cancel commands with invalid delimiters , ex. ls ;;; pwd or ls &&&& pwd
+  if ( c1 >= 2 || c2 >= 2 || c3 == 1){
+    printf("Ιnvalid command\n");
+    return 1;
+  }
+  // Cancel commands which start with ; or &&/&
+  if (line[0] == delims[0] || line[0] == delims[1]){
+    printf("Ιnvalid command\n");
+    return 1;
+  }
+
 
   int j=0;
   int i;
@@ -204,7 +222,6 @@ int consecutiveCmds(char *line , int ncmds){
   }else{
     fl = 0;
   }
-  //printf("%d\n", fl );
 
 
   token1 = strtok_r(line,linedelims[k],&saveptr);
@@ -212,7 +229,6 @@ int consecutiveCmds(char *line , int ncmds){
   stat = execute(args,fl);
   printf("\n");
   free(args);
-//  printf("%d\n", stat );
   if (stat == 0){
     return 1;
   }
@@ -253,7 +269,6 @@ int consecutiveCmds(char *line , int ncmds){
 }
 
 int redirectCmds(char **args){
-
 
   pid_t pid;
   int stat;
@@ -308,59 +323,132 @@ int redirectCmds(char **args){
     }
     exit(EXIT_SUCCESS);
 
+  //Error forking
   }else if( pid < 0){
     perror("myshell");
 
-  }
-  else{
+  //Parent process
+  }else{
     wait(&stat);
   }
 
   return 1;
 
-
-
-
-
 }
 
+//Main function
 int main(int argc, char const *argv[]) {
 
   char *line;
-  char **args;
   int status;
+  FILE *fp;
+  char **args;
+
   int ncmds; // Number of consecutive commands
-  int fl  = 1;
+  int fl  = 1; // Flag for communatication between processes
+
+  if(argc == 1){
+
+    do{
+
+      printf("kalantzis_8818>");
+      line = readLine();
 
 
-  do {
-    printf("kalantzis_8818>");
-    line = readLine();
+      //Redirection command
+      if(strchr(line,'<') != NULL | strchr(line,'>') != NULL){
+        args = splitLine(line);
+        status = redirectCmds(args);
+        continue;
 
-    if(strchr(line,'<') != NULL | strchr(line,'>') != NULL){
-      args = splitLine(line);
-      status = redirectCmds(args);
-      continue;
+      }
+      //Check for commands with special characters { ; , &&}
+      ncmds = nConsecutivecmds(line);
 
+      // Command without special character
+      if (ncmds == 0){
+        args = splitLine(line);
+        status = execute(args, fl);
+        free(line);
+        free(args);
+      // Command with special character
+      }else{
+
+        status = consecutiveCmds(line,ncmds);
+        free(line);
+
+      }
+
+
+    } while(status);
+
+
+  }else{
+
+    fp = fopen(argv[1], "r");
+    // File error
+    if (fp == NULL){
+      perror("Error opening file");
+      return 0;
     }
 
-    ncmds = nConsecutivecmds(line);
-    //printf("%d\n", ncmds );
-
-    if (ncmds == 0){
-      args = splitLine(line);
-      status = execute(args, fl );
-      free(line);
-      free(args);
-
-    }else{
-
-      status = consecutiveCmds(line,ncmds);
-      free(line);
-
+    line = malloc(sizeof(char) * BUF_SIZE);
+    // Allocation error
+    if (!line) {
+      fprintf(stderr, "myshell: allocation error\n");
+      exit(EXIT_FAILURE);
     }
 
-  } while (status);
+    fgets(line,BUF_SIZE,fp);
+
+    do{
+
+      // Skip newlines
+      if(strcmp(line,"\n") == 0) continue;
+
+      //Exit signal
+      if ( strcmp(line, "quit\n") == 0){
+        exit(EXIT_SUCCESS);
+      }
+
+      //Redirection command
+      if(strchr(line,'<') != NULL | strchr(line,'>') != NULL){
+        args = splitLine(line);
+        status = redirectCmds(args);
+        continue;
+
+      }
+      //Check for commands with special characters { ; , &&}
+      ncmds = nConsecutivecmds(line);
+
+      // Command without special character
+      if (ncmds == 0){
+        args = splitLine(line);
+        status = execute(args, fl );
+        free(line);
+        free(args);
+      // Command with special character
+      }else{
+
+        status = consecutiveCmds(line,ncmds);
+        free(line);
+
+      }
+
+      line = malloc(sizeof(char) * BUF_SIZE);
+      // Allocation error
+      if (!line) {
+        fprintf(stderr, "myshell: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+
+
+    }while(fgets(line,BUF_SIZE,fp) != NULL);
+
+    fclose(fp);
+    free(line);
+
+  }
 
   return 0;
 }
